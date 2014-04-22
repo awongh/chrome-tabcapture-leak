7ee513b2c7194201e0e9306963dc5cedf099a6d2
awongh <akira@awongh.com>
2014-04-22 04:09:47 -0700

    narrowing the leak

--- popup.js
+++ popup.js
@@ -1,14 +1,7 @@
 chrome.tabs.onActivated.addListener(function( activeInfo ){
   var windowId = activeInfo.windowId;
 
-  chrome.tabs.captureVisibleTab(windowId, undefined, function( blob ){
-    if( blob == undefined ) return;
-
-    var f = document.createElement( "img" );
-
-    f.src = blob;
-
-    f = null;
+  chrome.tabs.captureVisibleTab(windowId, undefined , function(blob){
     blob = null;
   });
 });
